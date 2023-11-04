39dbcd3beb47f6cbb6ea2ef2ed184427b3982adc
venkateshkkkk <124691574+venkateshkkkk@users.noreply.github.com>
2023-11-04 15:58:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
  region = "us-east-2"
- access_key = "AKIAXNJHMXFKEOLPT2VU"
- secret_key = "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"
+ access_key = "AKIAXNJHMXFKB5PJOR7O"
+ secret_key = "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"
 }
-resource "aws_instance" {
+resource "aws_instance" "dev" {
  ami = "ami-0e83be366243f524a"
  instance_type = "t2.micro"
  count = 2
 tags = {
-        Name = "integration"
+        Name = "sucess"
 }
 }
-
