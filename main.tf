7b9263072a18a7b5541c6ac07118a21edb2ca129
venkateshkkkk <124691574+venkateshkkkk@users.noreply.github.com>
2024-01-19 10:01:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,1 @@
-provider "aws" {
- region = "us-east-2"
- access_key = "AKIAXNJHMXFKB5PJOR7O"
- secret_key = "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"
-}
-resource "aws_instance" "dev" {
- ami = "ami-0e83be366243f524a"
- instance_type = "t2.micro"
- count = 2
-tags = {
-        Name = "sucess"
-}
-}
+hello alll good
